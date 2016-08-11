82a49b1e4e0bb9c438fa26488facf05c1f75e59f
unknown <ebner@GFA-CINTEG01.psi.ch>
2016-08-11 11:15:28 +0200

    changes

--- epics-base/bld.bat
+++ epics-base/bld.bat
@@ -7,40 +7,45 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd6
 REM    --------------- EPICS -----------------------------
 REM    -- R3.14 requirements
 set EPICS_HOST_ARCH=windows-x64
-rem set PATH=%PATH%;%LIBRARY_PREFIX%\epics\base-3.14.12.4\bin\%EPICS_HOST_ARCH%
+REM set PATH=%PATH%;%LIBRARY_PREFIX%\epics\base-3.14.12.4\bin\%EPICS_HOST_ARCH%
 
 
 echo %CYGWIN_PREFIX%
 
 set PREFIX=%CYGWIN_PREFIX%
 set EPICS_BASE=/cygdrive/c/Anaconda3/conda-bld/work/base-3.14.12.4
-rem set INSTALL_LOCATION=%CYGWIN_PREFIX%/epics
+REM set INSTALL_LOCATION=%CYGWIN_PREFIX%/epics
 
 echo %LIBRARY_PREFIX%
 echo %EPICS_HOST_ARCH%
 echo %INSTALL_LOCATION%
 
-rem set > condaEnv.txt
+REM Save current environment
+set > condaEnv.txt
 
-set ARCH=
-set LIBRARY_LIB=
-set LIBRARY_PREFIX=
-set HOMEPATH=
-set SP_DIR=
-set RECIPE_DIR=
-set PROMPT=$P$G
-set R=
-set LIBRARY_INC=
-set STDLIB_DIR=
-set SRC_DIR=
- 
- 
+REM Unset all environment variables
+
+REM set ARCH=
+REM set LIBRARY_LIB=
+REM set LIBRARY_PREFIX=
+REM set HOMEPATH=
+REM set SP_DIR=
+REM set RECIPE_DIR=
+REM set PROMPT=$P$G
+REM set R=
+REM set LIBRARY_INC=
+REM set STDLIB_DIR=
+REM set SRC_DIR=
+
 for /f "tokens=1* delims==" %%a in ('set') do (
 set %%a=
 )
+
+pause
+
 echo ##############################################################################################################
 echo "Write"
-for /f "delims=" %%a in (C:\Anaconda3\conda-bld\condaEnvW.txt) do (
+for /f "delims=" %%a in (C:\Users\ebner\conda-recipes\epics-base\condaEnvW.txt) do (
 set "%%a"
 )
 
@@ -48,16 +53,9 @@ echo ###########################################################################
 echo %PATH%
 echo ##############################################################################################################
 
+REM set PATH=%PATH%;C:\cygwin\bin
+REM set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x64
+REM set INCLUDE=%INCLUDE%;C:\Program Files (x86)\Windows Kits\8.1\Include\um;C:\Program Files (x86)\Windows Kits\8.1\Include\shared
+REM set EPICS_HOST_ARCH=windows-x64
 
-
-rem call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
-
-rem set PATH=%PATH%;C:\cygwin\bin
-rem set PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x64
-rem set INCLUDE=%INCLUDE%;C:\Program Files (x86)\Windows Kits\8.1\Include\um;C:\Program Files (x86)\Windows Kits\8.1\Include\shared
-rem set EPICS_HOST_ARCH=windows-x64
-
-make 
-
-cd
-pause
+make
