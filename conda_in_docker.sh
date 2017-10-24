82959fe475d69268ebca9e28ea0503a7c1fbd4e1
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-10-24 22:37:55 +0200

    install system gcc

--- conda_in_docker.sh
+++ conda_in_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker exec psi_conda bash -c "source /opt/docker/bin/entrypoint_source;cd /tmp/recipes;if [ ! -z "${CONDA_PY}" ]; then export CONDA_PY=${CONDA_PY}; fi;conda $*"
+docker exec psi_conda bash -c "cd /tmp/recipes;if [ ! -z "${CONDA_PY}" ]; then export CONDA_PY=${CONDA_PY}; fi;conda $*"
