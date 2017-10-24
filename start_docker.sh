9a585c8585ebbc9f876d4d1997a6808db45a50cf
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-10-24 22:17:29 +0200

    use anaconda perl package
    in docker images, perl might not be installed

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 docker run -dt --name "psi_conda" -v `pwd`:/tmp/recipes -v ${1}:/opt/conda/conda-bld condaforge/linux-anvil:latest
+docker exec psi_conda sudo yum install -y perl
