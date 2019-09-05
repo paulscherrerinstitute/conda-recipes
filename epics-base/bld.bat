2d20a7440b97a5207bf4a94368faf524b43d46c2
Wang Xiaoqiang <xiaoqiang.wang@psi.ch>
2019-09-05 14:36:38 +0200

    export EPICS_BASE and EPICS_HOST_ARCH

--- epics-base/bld.bat
+++ epics-base/bld.bat
@@ -18,3 +18,21 @@ echo Building at %CD%
 mingw32-make clean
 mingw32-make CROSS_COMPILER_TARGET_ARCHS=%EPICS_HOST_ARCH%-static
 
+# deal with env export
+mkdir %PREFIX%\etc\conda\activate.d
+mkdir %PREFIX%\etc\conda\deactivate.d
+
+set ACTIVATE=%PREFIX%\etc\conda\activate.d\epics_base.bat
+set DEACTIVATE=%PREFIX%\etc\conda\deactivate.d\epics_base.bat
+
+# set up
+echo @set "EPICS_BASE=%PREFIX%/epics" >> %ACTIVATE%
+echo @set "EPICS_HOST_ARCH=%EPICS_HOST_ARCH%" >> %ACTIVATE%
+
+# tear down
+echo @set "EPICS_BASE=" >> %DEACTIVATE%
+echo @set "EPICS_HOST_ARCH=" >> %DEACTIVATE%
+
+# clean up after self
+set ACTIVATE=
+set DEACTIVATE=
