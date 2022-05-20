17b7346285544a28c4a8f51832474b194d559297
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2022-05-20 09:41:04 +0200

    update condaforge build docker

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -dt --name "psi_conda" -e HOST_USER_ID=`id -u` -e HOST_GROUP_ID=`id -g` -v `pwd`:/tmp/recipes -v ${1}:/opt/conda/conda-bld condaforge/linux-anvil:latest
+docker run -dt --name "psi_conda" -e HOST_USER_ID=`id -u` -e HOST_GROUP_ID=`id -g` -v `pwd`:/tmp/recipes -v ${1}:/opt/conda/conda-bld condaforge/linux-anvil-comp7:latest
