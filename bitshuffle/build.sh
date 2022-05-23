ed744117088d07f2f37e189e9eff19c7a4a77184
ebner <simon.ebner@psi.ch>
2022-05-23 12:12:57 +0200

    fix build for linux

--- bitshuffle/build.sh
+++ bitshuffle/build.sh
@@ -1,2 +1,8 @@
 export CONDA_HOME=$PREFIX
+
+# Make sure package is not compiled with OpenMP
+cat > setup.cfg << EOF
+[build_ext]
+omp = 0
+EOF
 $PYTHON setup.py install     # Python command to install the script
