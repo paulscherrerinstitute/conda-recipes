1df3052e288f579350e019425e9b4465b6b27fb1
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2023-03-28 10:23:36 +0200

    derive EPICS_HOST_ARCH from machine type

--- cachannel/build.sh
+++ cachannel/build.sh
@@ -2,13 +2,21 @@
 export EPICS_BASE=$PREFIX/epics
 
 PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+MACHINE=$(uname -m)
 if [ $PLATFORM == "linux" ] ; then
-  export EPICS_HOST_ARCH=$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)
+  export EPICS_HOST_ARCH=linux-$MACHINE
 elif [ $PLATFORM == "darwin" ] ; then
-  export EPICS_HOST_ARCH=darwin-x86
+  if [ $MACHINE == "arm64" ]; then
+    export EPICS_HOST_ARCH=darwin-aarch64
+  elif [ $MACHINE == "x86_64" ]; then
+    export EPICS_HOST_ARCH=darwin-x86
+  else
+    echo "macOS CPU type '$MACHINE' not recognized"
+    exit 1
+  fi
 fi
 
 echo Using EPICS_BASE=$EPICS_BASE
 echo Using EPICS_HOST_ARCH=$EPICS_HOST_ARCH
 
-$PYTHON setup.py install
+$PYTHON -m pip install .
